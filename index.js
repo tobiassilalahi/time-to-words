e39f7f7abe66c77bdafb879cfe7222d2c856a309
YANSIN <yansin.huang1@unimelb.edu.au>
2024-11-13 22:11:47 +1100

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 // expecting time to be a string in the format like '8:15' or '12:30'
 function convertTimeToWords(time) {
-  // TODO: real code goes here!
-  if (time === '0:00') {
-    return 'midnight';
+  const array_time = time.split(':');
+  let hour = parseInt(array_time[0]);
+  let min = parseInt(array_time[1]);
+  
+  const dict = {
+    "1": "one", "2": "two", "3": "three", "4": "four", "5": "five", "6": "six", "7": "seven", "8": "eight", "9": "nine", "10": "ten", "11": "eleven", "12": "twelve", "13": "thirteen", "14": "fourteen", "15": "quarter", "16": "sixteen", "17": "seventeen", "18": "eighteen", "19": "nineteen", "20": "twenty",
+    "21": "twenty-one", "22": "twenty-two", "23": "twenty-three", "24": "twenty-four", "25": "twenty-five", "26": "twenty-six", "27": "twenty-seven", "28": "twenty-eight", "29": "twenty-nine", "30": "half"
+  };
+
+  if (min === 0) {
+    if (hour === 0) {
+      return 'midnight';
+    } else if (hour === 12) {
+      return 'midday';
+    } else {
+      return dict[hour.toString()].concat(" o'clock");
+    }
   }
 
-  return 'half past eight';
-}
+  if (min <= 30) {
+    return dict[min].concat(' past ').concat(dict[hour]);
+  } else {
+    return dict[60-min].concat(' to ').concat(dict[hour+1]);
+  }
 
+
+}
 module.exports = { convertTimeToWords };
--- index.test.js
+++ index.test.js
@@ -15,4 +15,20 @@ describe('Time to words', () => {
     const timeInWords = convertTimeToWords('2:45');
     expect(timeInWords).toBe('quarter to three');
   });
+
+  it('Handles times before 30 mins - 2:23', () => {
+    const timeInWords = convertTimeToWords('2:23');
+    expect(timeInWords).toBe('twenty-three past two');
+  });
+
+  it('Handles times after 30 mins non quarter - 2:48', () => {
+    const timeInWords = convertTimeToWords('2:48');
+    expect(timeInWords).toBe('twelve to three');
+  });
+
+  it('Handles o clock - 4:00', () => {
+    const timeInWords = convertTimeToWords('4:00');
+    expect(timeInWords).toBe("four o'clock");
+  });
+
 });
